ca41a519d72ee0c2452b65d3d3e3d214225aa47a
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2019-11-30 12:44:38 -0200

    Update on gulpfile to work with babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,24 @@
 const { src, dest , series} = require('gulp')
 const { exec } = require('child_process')
+const babel = require('gulp-babel')
 
-const compile = async () => {
+const compile = () => {
     const compileImperative = src('promise-rejection-imperative.js')
-          .pipe(dest('dist/'))
+    	.pipe(babel)
+        .pipe(dest('dist/'))
 
-    const copyFunctional = async () = {
-    	await exec('cp promise-rejection-functional.js dest/')
-    }
+    const copyFunctional = () => src('promise-rejection-functional.js')
+    	.pipe(dest('dist/'))
 }
 
 const start = async () => {
-	await exec('node dist/promise-rejection-imperative.js')
-	await exec('node dist/promise-rejection-functionla.js')
+	await exec('node dist/promise-rejection-imperative.js', {
+		cwd: process.cwd()
+	})
+
+	await exec('node dist/promise-rejection-functionla.js', {
+		cwd: process.cwd()
+	})
 }
 
 export.default = series(compile, start)
