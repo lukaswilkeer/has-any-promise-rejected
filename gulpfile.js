51236ec5027c902cc6e7debb19a661a60186c6cf
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2019-12-12 21:08:18 -0200

    Better gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,25 @@ const { src, dest , series} = require('gulp')
 const { exec } = require('child_process')
 const babel = require('gulp-babel')
 
-const compile = () => {
-    const compileImperative = src('promise-rejection-imperative.js')
-    	.pipe(babel())
-      .pipe(dest('dist/'))
+const compileImperative = () => {
+  return src('promise-rejection-imperative.js')
+    .pipe(babel())
+    .pipe(dest('dist/'))
+}
 
-    const copyFunctional = src('promise-rejection-functional.js')
-    	.pipe(dest('dist/'))
+const copyFunctional = () => {
+  return src('promise-rejection-functional.js')
+    .pipe(dest('dist/'))
 }
 
 const start = async () => {
-	await exec('node dist/promise-rejection-imperative.js', {
-		cwd: process.cwd()
-	})
+  await exec('node dist/promise-rejection-imperative.js', {
+    cwd: process.cwd()
+  })
 
-	await exec('node dist/promise-rejection-functionla.js', {
-		cwd: process.cwd()
-	})
+  await exec('node dist/promise-rejection-functionla.js', {
+    cwd: process.cwd()
+  })
 }
 
-exports.default = series(compile, start)
+exports.default = series(compileImperative, copyFunctional, start)
