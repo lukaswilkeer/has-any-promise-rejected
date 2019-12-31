769594dbe351af3cb2765b3d3041d0a330d3f068
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2019-12-31 20:54:07 -0200

    imrpove imperative formula with for..await

--- promise-rejection-imperative.js
+++ promise-rejection-imperative.js
@@ -8,38 +8,19 @@ const urls = [
   'http://localhost:3000/nowhere'
  ]
 
-const generatePromises = (url) => {
-  return axios(url).catch(null)
+const generatePromises = (urls) => {
+  return urls.map((url) => axios(url).then((result) => result)
+    .catch((err) => null))
 }
 
-const promises = urls.map(generatePromises)
-
-const mapPromises = async (promises, index = 0) => {
-	let result;
-
-	try {
-	  result = await promises[index].catch((err) => null)
-	} catch (err) {
-		 reuslt = err
-	}
-
- 	results.push(result)
-
-	return index < promises.length - 1
-		? mapPromises(promises, ++index)
-		: results
-}
-
-(async (arrayOfPromises) => {
+(async (promises) => {
   // here we have the problem of Promise.all rejection
   // const result = Promise.all(arrayOfPromises)
-  // so, to resolve this problem of rejections we use fow await with babel,
-  // but there's a problem. for await transform the code to async generetors
-  // but you cannot yield inside a promise or yield a promise inside a generator, causing a parse error.
-  // on other hand, you can implement the generator by itself, but the root cause is the same.
+  // so, to resolve this problem of rejections we use fow await.
 
-  const resolvedPromises = await mapPromises(arrayOfPromises)
-  const haveSomePromiseFailed = resolvedPromises.filter(result => result === null).length > 0
+  for await (const promise of promises) results.push(promise)
+
+  const haveSomePromiseFailed = results.filter(result => result === null).length > 0
 
   assert(haveSomePromiseFailed, true)
-})(promises)
+})(generatePromises(urls))
